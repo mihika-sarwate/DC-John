8f46ffdb48182d41b3b94022c0b0a8e85e58084b
Mihika Sarwate <mihika@example.com>
2026-02-06 21:54:32 +0530

    Build static site for GitHub Pages - remove blog dynamic route

--- docs/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"XFNKM1NjSmh9X7oA1WvSaeFLGax9d3kmvoR9hGPyAzE=\"\n}"
--- docs/dev/static/chunks/app_page_tsx_05d61800._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_3e86021b._.js",
-  "static/chunks/components_1ed83a90._.js"
-],
-    source: "dynamic"
-});
